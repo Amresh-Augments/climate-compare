5f4342cb47d02d0f31639bbb4cfc64710a74f714
abdulladone94 <abdullaf@booleanlabs.biz>
2024-08-10 15:17:09 +0530

    Completed TASK 2.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,22 @@
-import { COUNTRIES, USER } from "./data.js";
+import { COUNTRIES, USER } from './data.js';
 
-import NodeCache from "node-cache";
-import bodyparser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import { generateClimateData } from "./helpers.js";
-import jsonwebtoken from "jsonwebtoken";
+import NodeCache from 'node-cache';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { generateClimateData } from './helpers.js';
+import jsonwebtoken from 'jsonwebtoken';
 
 const { sign, verify } = jsonwebtoken;
 
 const { json } = bodyparser;
 
 // CODE FOR TASK 2.2 -------------------------------------------
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // END OF CODE FOR TASK 2.2 ------------------------------------
 
@@ -25,23 +29,20 @@ const secretKey = process.env.SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
   // CODE FOR TASK 2.4 -------------------------------------------
-
   // END OF CODE FOR TASK 2.4 ------------------------------------
 };
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   // CODE FOR TASK 2.5 -------------------------------------------
-
   // END OF CODE FOR TASK 2.5 ------------------------------------
 });
 
-app.get("/climate/:country", verifyToken, (req, res) => {
+app.get('/climate/:country', verifyToken, (req, res) => {
   // CODE FOR TASK 2.6 -------------------------------------------
-  
   // END OF CODE FOR TASK 2.6 ------------------------------------
 });
 
-app.get("/countries", verifyToken, (req, res) => {
+app.get('/countries', verifyToken, (req, res) => {
   return res.json(COUNTRIES);
 });
 
